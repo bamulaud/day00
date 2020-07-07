0fc33eb6001973caaaf88c1b65d4562cf6c3a7e3
Baby Mulaudzi <bamulaud@c5r10s1.wethinkcode.co.za>
2020-07-07 10:51:40 +0200

    done

---
+++ ex01/email.c
@@ -0,0 +1,1 @@
+bamulaud@student.wethinkcode.co.za
---
+++ ex04/kerberos.c
@@ -0,0 +1,4 @@
+ Principal: bamulaud@WETHINKCODE.CO.ZA
+
+  Issued                Expires               Principal
+Jul  6 14:03:53 2020  Jul  7 00:03:53 2020  krbtgt/WETHINKCODE.CO.ZA@WETHINKCODE.CO.ZA
